c939fe7b5f24d92ca03603d3eb09686b31d919b0
Christopher Robert Philabaum <chaos7theory@live.com>
2016-05-26 04:39:32 -0700

    Added entity state handling
    Added ability to differentiate between walking, attacking, and standing still.

--- src/game.js
+++ src/game.js
@@ -9,10 +9,11 @@
     var RENDER_WIDTH, RENDER_HEIGHT;
 
     var tu = new TileUtilities(PIXI);
+    var game;
 
     // Only run when the document is fully loaded.
     document.addEventListener("DOMContentLoaded", (event) => {
-        let game = new Game(render);
+        game = new Game(render);
 
         function render() {
             game.update();
@@ -65,32 +66,46 @@
                         switch(e.keyCode) {
                             case(65):
                             case(68):
-                                if(!this.player.isAlive || this.player.moving || e.repeat)
+                                e.preventDefault();
+                                if(!this.player.isAlive || this.player.moving) {
                                     return;
+                                }
+                                else {
+                                    this.player.move_dir = MOVE_DIR.NONE;
+                                }
                         }
 
-                        this.player.move_dir = DIRECTION.NONE;
                         switch(e.keyCode) {
+                            // A
                             case(65):
-                                e.preventDefault();
-                                this.player.move_dir = DIRECTION.LEFT;
+                                this.player.move_dir = MOVE_DIR.LEFT;
                                 break;
+                            // D
                             case(68):
+                                this.player.move_dir = MOVE_DIR.RIGHT;
+                                break;
+                            // Space
+                            case(32):
                                 e.preventDefault();
-                                this.player.move_dir = DIRECTION.RIGHT;
+                                this.player.punch();
                                 break;
                         }
 
-                        this.player.move(1);
+                        this.player.move();
                     });
 
                     document.addEventListener('keyup', e => {
                         switch(e.keyCode) {
+                            // A
                             case(65):
+                            // D
                             case(68):
+                                this.player.move_dir = MOVE_DIR.NONE;
+                                break;
+                            // Space
+                            case(32):
                                 e.preventDefault();
                         }
-                        this.player.move_dir = DIRECTION.NONE;
                     });
 
                     // Initialize render loop
@@ -99,26 +114,92 @@
         }
 
         update() {
+            this.moveCamera();
+            this.player.update()
             // Final step
             this.renderer.render(this.screenMap.get(this.currentScreen));
         }
 
         moveCamera() {
-            let x = -player.sprite.x * ZOOM + RENDER_WIDTH / 2 - player.sprite.width / 2 * ZOOM;
+            let x = -this.player.sprite.x * ZOOM + RENDER_WIDTH / 2 - this.player.sprite.width / 2 * ZOOM;
 
-            this.screenMap.get('main').x = -Match.max(0, Math.min(this.world.worldWidth * ZOOM - RENDER_WIDTH, -x));
+            this.screenMap.get('main').x = -Math.max(0, Math.min(this.world.worldWidth * ZOOM - RENDER_WIDTH, -x));
         }
     }
 
     // Abstract class
     class Entity {
-        constructor(max, speed) {
+        constructor(spawn, max, speed, dir, file_pre) {
             this.moving = false;
+            this.punching = false;
             this.speed = speed;
             this.MAX_HEALTH = max;
             this.health = this.MAX_HEALTH;
             this.move_dir = DIRECTION.NONE;
-            this.direction = DIRECTION.NONE;
+            this.direction = dir;
+
+            this.sprite = new PIXI.Container();
+            this.sprite.x = spawn.x;
+            this.sprite.y = spawn.y;
+
+            this.state = STATE.STILL;
+            this.direction = DIRECTION.RIGHT;
+
+            this.direction_containers = new Array();
+            for(let dir in DIRECTION) {
+                this.direction_containers.push(new PIXI.Container());
+            }
+
+            this.states = new Array();
+            for(let dir of this.direction_containers) {
+                let arr = new Array();
+                for(let s in STATE) {
+                    arr.push(new Array());
+                }
+                this.states.push(arr);
+            }
+
+            this.states[DIRECTION.RIGHT][STATE.STILL] = new PIXI.Sprite(PIXI.Texture.fromFrame(`${file_pre}1.png`));
+            this.states[DIRECTION.LEFT][STATE.STILL] = new PIXI.Sprite(PIXI.Texture.fromFrame(`${file_pre}2.png`));
+            let frames = new Array();
+            for(let f = 3; f <= 6; f++) {
+                frames.push(PIXI.Texture.fromFrame(`${file_pre}${f}.png`));
+            }
+            this.states[DIRECTION.RIGHT][STATE.WALK] = new PIXI.extras.MovieClip(frames);
+            this.states[DIRECTION.RIGHT][STATE.WALK].animationSpeed = 0.25;
+            this.states[DIRECTION.RIGHT][STATE.WALK].play();
+
+            frames = new Array();
+            for(let f = 7; f <= 10; f++) {
+                frames.push(PIXI.Texture.fromFrame(`${file_pre}${f}.png`));
+            }
+            this.states[DIRECTION.LEFT][STATE.WALK] = new PIXI.extras.MovieClip(frames);
+            this.states[DIRECTION.LEFT][STATE.WALK].animationSpeed = 0.25;
+            this.states[DIRECTION.LEFT][STATE.WALK].play();
+
+            frames = new Array();
+            for(let f = 11; f <= 12; f++) {
+                frames.push(PIXI.Texture.fromFrame(`${file_pre}${f}.png`));
+            }
+            this.states[DIRECTION.RIGHT][STATE.ATTACK] = new PIXI.extras.MovieClip(frames);
+            this.states[DIRECTION.RIGHT][STATE.ATTACK].animationSpeed = 0.1;
+            this.states[DIRECTION.RIGHT][STATE.ATTACK].loop = false;
+
+            frames = new Array();
+            for(let f = 13; f <= 14; f++) {
+                frames.push(PIXI.Texture.fromFrame(`${file_pre}${f}.png`));
+            }
+            this.states[DIRECTION.LEFT][STATE.ATTACK] = new PIXI.extras.MovieClip(frames);
+            this.states[DIRECTION.LEFT][STATE.ATTACK].animationSpeed = 0.1;
+            this.states[DIRECTION.RIGHT][STATE.ATTACK].loop = false;
+
+            for(let dir in DIRECTION) {
+                for(let state in STATE) {
+                    this.direction_containers[DIRECTION[dir]].addChild(this.states[DIRECTION[dir]][STATE[state]]);
+                }
+
+                this.sprite.addChild(this.direction_containers[DIRECTION[dir]]);
+            }
         }
 
         hurt(dmg) {
@@ -132,6 +213,41 @@
         jump() {};
         walk() {};
         die() {};
+        update() {
+            if(this.punching && this.state !== STATE.ATTACK) {
+                this.state = STATE.ATTACK;
+            }
+            else if(this.moving) {
+                this.state = STATE.WALK;
+            }
+            else {
+                this.state = STATE.STILL;
+            }
+            switch(this.move_dir) {
+                case(MOVE_DIR.LEFT):
+                    this.direction = DIRECTION.LEFT;
+                    break;
+                case(MOVE_DIR.RIGHT):
+                    this.direction = DIRECTION.RIGHT;
+                    break;
+            }
+
+            for(let dir in DIRECTION) {
+                this.direction_containers[DIRECTION[dir]].visible = DIRECTION[dir] === this.direction;
+                for(let state in STATE) {
+                    this.states[DIRECTION[dir]][STATE[state]].visible = STATE[state] === this.state;
+                }
+            }
+
+            if(this.state === STATE.ATTACK) {
+                this.states[this.direction][this.state].play();
+                this.punching = false;
+            }
+        };
+
+        punch() {
+            this.punching = true;
+        }
 
         get isAlive() {
             return this.health > 0;
@@ -151,40 +267,37 @@
         }
 
         move() {
-            console.log(this.move_dir);
-            if(this.move_dir === DIRECTION.NONE) {
+            if(!this.move_dir || this.move_dir === MOVE_DIR.NONE) {
                 this.moving = false;
                 return;
             }
 
             this.moving = true;
             switch(this.move_dir) {
-                case(DIRECTION.LEFT):
+                case(MOVE_DIR.LEFT):
                     createjs.Tween.get(this.sprite).to({x: this.sprite.x - this.speed * TILE_SIZE},
                         500)
-                        .call(this.move);
+                        .call(() => {
+                            this.move();
+                        });
                     break;
-                case(DIRECTION.RIGHT):
+                case(MOVE_DIR.RIGHT):
                     createjs.Tween.get(this.sprite).to({x: this.sprite.x + this.speed * TILE_SIZE},
                         500)
-                        .call(this.move);
+                        .call(() => {
+                            this.move();
+                        });
             }
         }
     }
 
     class Player extends Entity {
         constructor(spawn) {
-            super(100, 1);
-            // #TODO Load player sprite from frame
-            this.sprite = new PIXI.Container();
-            this.sprite.x = spawn.x;
-            this.sprite.y = spawn.y;
+            super(spawn, 100, 1.5, DIRECTION.RIGHT, 'player');
+        }
 
-            this.direction = DIRECTION.RIGHT;
-
-            this.stillAnim = new PIXI.Sprite(PIXI.Texture.fromFrame('player1.png'));
-            this.sprite.addChild(this.stillAnim);
-            let frames = new Array();
+        update() {
+            super.update();
         }
 
         jump() {
@@ -207,15 +320,16 @@
 
     class Enemy extends Entity {
         constructor(enemy_spawn, max) {
-            super(max, 1 / 4)
-            // #TODO Load enemy sprite.
-            this.sprite = new PIXI.Sprite();
-            this.direction = DIRECTION.LEFT;
+            super(enemy_spawn, max, 1 / 4, DIRECTION.LEFT, 'enemy');
         }
 
         // Override
         attack(player) {
             player.hurt(5);
+        }
+
+        update() {
+            super.update();
         }
 
         follow(player) {
@@ -225,10 +339,21 @@
         }
     }
 
-    const DIRECTION = {
+    const STATE = {
+        STILL: 0,
+        WALK: 1,
+        ATTACK: 2
+    }
+
+    const MOVE_DIR = {
         NONE: 0,
         LEFT: 1,
         RIGHT: 2
+    }
+
+    const DIRECTION = {
+        LEFT: 0,
+        RIGHT: 1
     }
 
     function getRandomInt(min, max) {
