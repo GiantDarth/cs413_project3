975dca63520ce8eab5bc430160f25f2b8b43c7ca
Christopher Robert Philabaum <chaos7theory@live.com>
2016-05-26 22:34:06 -0700

    Added '!' to title

--- src/game.js
+++ src/game.js
@@ -26,7 +26,7 @@
             RENDER_HEIGHT = TILE_SIZE * ZOOM * MAP_HEIGHT;
             RENDER_WIDTH = TILE_SIZE * ZOOM * TILE_VIEW;
 
-            this.title = "Beat-'em-All";
+            this.title = "Beat-'em-All!";
 
             // Append renderer to gameport
             this.gameport = document.getElementById("gameport");
